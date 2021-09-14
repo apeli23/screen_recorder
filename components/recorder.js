ca590af70d130abacb64fdca12d026d9aacb3338
breezy <apelibrian@gmail.com>
2021-09-14 17:45:42 +0300

    full ui

--- components/recorder.js
+++ components/recorder.js
@@ -1,5 +1,6 @@
 import React, { useRef } from "react";
 import useScreenRecorder from "use-screen-recorder";
+import Button from "@material-ui/core/Button"
 
 function Recorder() {
     const videoRef = useRef();
@@ -14,12 +15,59 @@ function Recorder() {
         stopRecording,
     } = useScreenRecorder({ audio: true });
 
+    
+    function uploadVideo() {
+        var reader = new FileReader();
+        reader.readAsDataURL(blobUrl);
+        reader.onloadend = function () {
+            var base64data = reader.result;
+            console.log(base64data);
+        }
+    }
+
     return (
         <div>
             <h1 className='elegantshadow'>Screen Recorder</h1>
             <div className='status'>
-                Status: {status}<br/><br/>
+                Status: {status}<br /><br />
                 Video Link: {blobUrl || "Waiting..."}
+            </div>
+            <div>
+                <video
+                    ref={videoRef}
+                    src={blobUrl}
+                    controls
+                    autoPlay
+                />
+            </div>
+            <div className='buttons'>
+                {(status === "idle" || status === "error") && (
+                    <Button onClick={startRecording} variant='contained' color='primary' >Start recording</Button>
+                )}
+                {(status === "recording" || status === "paused") && (
+                    <Button onClick={stopRecording} variant='contained' color='primary'>Stop recording</Button>
+                )}{' '}
+                {(status === "recording" || status === "paused") && (
+                    <Button
+                        onClick={() =>
+                            status === "paused" ? resumeRecording() : pauseRecording()
+                        }
+                        variant='contained' color='primary'
+                    >
+                        {status === "paused" ? "Resume recording" : "Pause recording"}
+                    </Button>
+                )}
+                {status === "stopped" && (
+                    <Button
+                        onClick={() => {
+                            resetRecording();
+                            videoRef.current.load();
+                        }}
+                        variant='contained' color='primary'
+                    >
+                        Reset recording
+                    </Button>
+                )}
             </div>
         </div>
     )
