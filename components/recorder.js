0690b972f611643de0884d6794e63ac3a58b19b7
breezy <apelibrian@gmail.com>
2021-09-14 16:25:35 +0300

    updated ui

--- components/recorder.js
+++ components/recorder.js
@@ -1,7 +1,26 @@
-function Recorder(){
-    return(
+import React, { useRef } from "react";
+import useScreenRecorder from "use-screen-recorder";
+
+function Recorder() {
+    const videoRef = useRef();
+
+    const {
+        blobUrl,
+        pauseRecording,
+        resetRecording,
+        resumeRecording,
+        startRecording,
+        status,
+        stopRecording,
+    } = useScreenRecorder({ audio: true });
+
+    return (
         <div>
-            works
+            <h1 className='elegantshadow'>Screen Recorder</h1>
+            <div className='status'>
+                Status: {status}<br/><br/>
+                Video Link: {blobUrl || "Waiting..."}
+            </div>
         </div>
     )
 } export default Recorder;
